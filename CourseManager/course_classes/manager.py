f669289feb314f3bbb7cb79eb6d541d8545f4d4d
ReznikovRoman <romanreznikov2002@yandex.ru>
2020-10-25 21:23:03 +0300

    Reformat code

--- CourseManager/course_classes/manager.py
+++ CourseManager/course_classes/manager.py
@@ -7,22 +7,7 @@ from CourseManager.course_classes.enroll import Enroll
 
 class Manager(Person):
     courses_lst = []
-    """
-    Manager, who can create new Courses and edit them
-    
-    Args:
-        first_n (str): Is used to determine a first name of a Manager
-        last_n (str): Is used to determine a last name of a Manager
-        phone (str): Is used to determine a phone number of a Manager
-        date_of_birth (datetime.date): Is used to determine a date of birth of a Manager
-        address (Address): Is used to determine an Address of a Manager
-        salary (int): Is used to determine a salary of a Manager
-        
-    Attributes:
-        __salary (int) : Stores salary
-        __courses (int) : Stores Courses, which Manager can edit
-        __permissions (str) : Stores Manager's permissions
-    """
+    """ Manager, who can create new Courses or change their names """
     def __init__(self, first_n, last_n, phone, date_of_birth, address, salary):
         super(Manager, self).__init__(first_n, last_n, phone, date_of_birth, address)
         self.__salary = salary
@@ -30,92 +15,32 @@ class Manager(Person):
         self.__permissions = "manager"
 
     def add_course(self, course):
-        """
-        Manager can add Course to his 'List of Courses'
-
-        Args:
-            course (Course): Course, which will be added to the list
-
-        Returns:
-
-        """
         if course not in self.__courses:
             self.__courses.append(course)
 
     def create_course(self, course_name, course_id):
-        """
-        Creates a new Course
-
-        Args:
-            course_name (str): new Course's name
-            course_id (str): new Course's ID
-
-        Returns:
-            new_course (Course): Created Course
-        """
         new_course = Course(course_name, course_id, self.__permissions)
         if new_course not in Manager.courses_lst:
             Manager.courses_lst.append(new_course)
         return new_course
 
     def change_course_name(self, course: Course, new_name):
-        """
-        Changes a Course's name
-
-        Args:
-            course (Course): Course, which name will be changed
-            new_name (str): new Course's name
-
-        Returns:
-
-        """
         if course in self.__courses:
             course.change_name(new_name, self.__permissions)
 
     def change_course_min_grade(self, course: Course, new_grade):
-        """
-        Manager can change the Course's min grade
-
-        Args:
-            course (Course): Course, which min grade will be changed
-            new_grade (int): new Course's min grade
-
-        Returns:
-
-        """
         course.set_min_grade(new_grade, self.__permissions)
 
     def get_salary(self):
-        """
-        Returns:
-            __salary
-        """
         return self.__salary
 
     def get_manager_courses(self):
-        """
-        Returns:
-            __courses
-        """
         return self.__courses
 
     def get_permission(self):
-        """
-        Returns:
-            __permissions
-        """
         return self.__permissions
 
     def end_course(self, course):
-        """
-        Manager can finish the Course
-
-        Args:
-            course (Course): Course, which will be ended
-
-        Returns:
-
-        """
         if course.get_state() != "closed":
             course.change_state(new_state="closed", permissions=self.__permissions)
 
--- CourseManager/course_classes/person.py
+++ CourseManager/course_classes/person.py
@@ -3,24 +3,6 @@ from datetime import date
 
 
 class Person:
-    """
-    Represents a Person
-
-    Args:
-        first_name (str): Is used to determine a first name of a Person
-        last_name (str): Is used to determine a last name of a Person
-        phone (str): Is used to determine a phone number of a Person
-        date_of_birth (datetime.date): Is used to determine a date of birth of a Person
-        address (Address): Is used to determine an Address of a Person
-
-    Attributes:
-        __first_name (str): Stores first_name
-        __last_name (str): Stores last_name
-        __phone (str): Stores phone
-        __date_of_birth (datetime.date): Stores date_of_birth
-        __address (Address): Stores address
-
-    """
     def __init__(self, first_name, last_name, phone, date_of_birth: date, address):
         self.__first_name = first_name
         self.__last_name = last_name
@@ -29,49 +11,23 @@ class Person:
         self.__address = address
 
     def get_first_name(self):
-        """
-        Returns:
-            __first_name
-        """
         return self.__first_name
 
     def get_last_name(self):
-        """
-        Returns:
-            __last_name
-        """
         return self.__last_name
 
     def get_full_name(self):
-        """
-
-        Returns:
-            Full name of a Person (last name + first name)
-        """
         return f"{self.__last_name} {self.__first_name}"
 
     def get_phone(self):
-        """
-        Returns:
-            __phone
-        """
         return self.__phone
 
     def get_age(self):
-        """
-
-        Returns:
-            Age of a Person
-        """
         today = date.today()
         born = self.__date_of_birth
         return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
 
     def get_address(self):
-        """
-        Returns:
-            __address
-        """
         return self.__address
 
 
